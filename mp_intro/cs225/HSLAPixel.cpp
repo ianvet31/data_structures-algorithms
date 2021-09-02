73f33a290f35e844f090ab0844c3974a6cc4c7ec
Vetter, Ian Matthew <ivette2@illinois.edu>
2021-09-02 13:13:56 -0500

    Update HSLAPixel.cpp

--- mp_intro/cs225/HSLAPixel.cpp
+++ mp_intro/cs225/HSLAPixel.cpp
@@ -3,9 +3,31 @@
  * Implementation of the HSLAPixel class for use in with the PNG library.
  *
  * @author CS 225: Data Structures
+ * @version 2018r1-lab1
  */
 
 #include "HSLAPixel.h"
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 namespace cs225 {
+	HSLAPixel::HSLAPixel() {
+		h = 0.;
+		s = 0.;
+		l = 1.0;
+		a = 1.0;
+	}
+	HSLAPixel::HSLAPixel(double h_, double s_, double l_) {
+		h = h_;
+		s = s_;
+		l = l_;
+		a = 1.0;
+	}
+	HSLAPixel::HSLAPixel(double h_, double s_, double l_, double a_) {
+		h = h_;
+		s = s_;
+		l = l_;
+		a = a_;
+	}
 }
