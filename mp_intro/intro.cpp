fb265a1190d1977dd41b59fa6ed47a9bdadcf527
Ian Vetter <ivette2@linux-ssh-01.ews.illinois.edu>
2021-09-02 14:03:32 -0500

    Added rotate function

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -5,7 +5,18 @@
 
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+  PNG img;
+  img.readFromFile(inputFile);
+  PNG imgout(img.width(), img.height());
+
+  for (unsigned x = 0; x < img.width(); x++) {
+    for (unsigned y = 0; y < img.height(); y++) {
+      HSLAPixel & pixel = img.getPixel(x, y);
+      HSLAPixel & pixel2 = imgout.getPixel(img.width() - x - 1, img.height() - y - 1);
+      pixel2 = pixel
+    }
+  }
+  imgout.writeToFile(outputFile);
 }
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
