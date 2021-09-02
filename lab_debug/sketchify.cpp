7da73116d2beee53cfc3d0ffb7ac70b1b6a5e117
Ian Vetter <ivette2@linux-ssh-01.ews.illinois.edu>
2021-09-02 17:22:48 -0500

    Passed all tests

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -5,6 +5,7 @@
 #pragma GCC diagnostic ignored "-Wuninitialized"
 #include <cstdlib>
 #include <cmath>
+using namespace std;
 
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
@@ -29,29 +30,29 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
-    return &p;
+    HSLAPixel *p = new HSLAPixel(1, 0.8, 0.5); //fixed to return HSLAPixel pointer
+    return p;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
     PNG* original = NULL;
-
+    original = new PNG(); //needed to create a PNG for original before arrow
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
+    
 
     // Create out.png
     PNG* output;
-    setupOutput(width, height);
-
+    output = setupOutput(width, height); //initialized output PNG pointer
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor();
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; 0 < y && y < height; y++) {
+        for (unsigned x = 1; 0 < x && x < width; x++) { //change loop condition to x && x because x and y are unsigned
             // Calculate the pixel difference
             HSLAPixel& prev = original->getPixel(x - 1, y - 1);
             HSLAPixel& curr = original->getPixel(x, y);
@@ -59,9 +60,12 @@ void sketchify(std::string inputFile, std::string outputFile) {
 
             // If the pixel is an edge pixel,
             // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
+            HSLAPixel &currOutPixel = output->getPixel(x, y);
             if (diff > 20) {
-                currOutPixel = *myPixel;
+                currOutPixel.h = myPixel->h;  //copies all attributes from my pixel to current
+                currOutPixel.s = myPixel->s;
+                currOutPixel.a = myPixel->a;
+                currOutPixel.l = myPixel->l;
             }
         }
     }
